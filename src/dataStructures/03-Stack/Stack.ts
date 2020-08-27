fc673ae6568d11818e2e598b0d86ce6eab53a41d
Juliapp <aragaopintojuli@gmail.com>
2020-08-27 17:40:58 -0300

    :sparkles: Stacks

---
+++ src/dataStructures/03-Stack/Stack.ts
@@ -0,0 +1,90 @@
+import LinkedList from "../02-LinkedLists/LinkedList";
+import { EqualsFunction } from "../../utils";
+
+class Stack<T> implements Iterable<T> {
+    private list: LinkedList<T>;
+
+    constructor(equalsFunction?: utils.EqualsFunction<T>) {
+        if (equalsFunction) this.list = new LinkedList(equalsFunction);
+        else this.list = new LinkedList();
+    }
+
+    /*****************************************************************************
+                                    INSPECTION
+    *****************************************************************************/
+    /**
+     * Returns size of stack - O(1)
+     * @returns {number}
+     */
+    size(): number {
+        return this.list.size();
+    }
+
+    /**
+     * Returns true if stack is empty, false otherwise - O(1)
+     * @returns {number}
+     */
+    isEmpty(): boolean {
+        return this.list.isEmpty();
+    }
+
+    /**
+     * Deletes all elements in queue - O(1)
+     */
+    clear(): void {
+        this.list.clear();
+    }
+
+    /*****************************************************************************
+                                    INSERTION/DELETION
+    *****************************************************************************/
+    /**
+     * Pushes element onto the stack - O(1)
+     * @param {T} element - element to push on stack
+     */
+    push(element: T): void {
+        this.list.addBack(element);
+    }
+
+    /**
+     * Pops an element off the stack - O(1)
+     * @returns {T} - Element which was popped off
+     */
+    pop(): T | null {
+        if (this.isEmpty()) return null;
+        return this.list.removeBack();
+    }
+
+    /*****************************************************************************
+                                    ACCESSING
+    *****************************************************************************/
+    /**
+     * Peeks at the top most element on the stack - O(1)
+     * @returns {T} - Topmost element
+     */
+    peek(): T | null {
+        if (this.isEmpty()) return null;
+        return this.list.peekBack();
+    }
+
+    /*****************************************************************************
+                                    SEARCHING
+    *****************************************************************************/
+    /**
+     * Checks if value is in stack - O(n)
+     * @param {T} element  - element to search for
+     * @returns {boolean}
+     */
+    contains(element: T): boolean {
+        return this.list.contains(element);
+    }
+
+    /*****************************************************************************
+                                    HELPERS
+    *****************************************************************************/
+    [Symbol.iterator](): Iterator<T> {
+        return this.list[Symbol.iterator]();
+    }
+}
+
+export default Stack;
